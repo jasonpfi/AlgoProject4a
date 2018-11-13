520dd6f83641fc319f981f915db35ddd2fccdbe2
Sam Smucny <smucny.sam@gmail.com>
2018-11-12 23:55:28 -0500

    Finished debugging different indexing functions.

--- fitsmu-4a/fitsmu-4a.cpp
+++ fitsmu-4a/fitsmu-4a.cpp
@@ -12,9 +12,9 @@ typedef int ValueType; // The type of the value in a cell
 const int Blank = -1;  // Indicates that a cell is blank
 
 const int SquareSize = 3;  //  The number of cells in a small square
-                           //  (usually 3).  The board has
-                           //  SquareSize^2 rows and SquareSize^2
-                           //  columns.
+						   //  (usually 3).  The board has
+						   //  SquareSize^2 rows and SquareSize^2
+						   //  columns.
 
 const int BoardSize = SquareSize * SquareSize;
 
@@ -24,198 +24,219 @@ const int MaxValue = 9;
 int numSolutions = 0;
 
 class board
-// Stores the entire Sudoku board
+	// Stores the entire Sudoku board
 {
-   public:
-      board(int);
-      void clear();
-      void initialize(ifstream &fin);
-      void print();
-      bool isBlank(int, int);
-      ValueType getCell(int, int);
-      void setCell(int i, int j, int val);
-      void clearCell(int i, int j);
-      bool isSolved();
-	  void printConflicts() {}; // TODO
+public:
+	board(int);
+	void clear();
+	void initialize(ifstream &fin);
+	void print();
+	bool isBlank(int, int);
+	ValueType getCell(int, int);
+	void setCell(int i, int j, int val);
+	void clearCell(int i, int j);
+	bool isSolved();
+	void printConflicts();
 
-   private:
+private:
 
-      // The following matrices go from 1 to BoardSize in each
-      // dimension, i.e., they are each (BoardSize+1) * (BoardSize+1)
+	// The following matrices go from 1 to BoardSize in each
+	// dimension, i.e., they are each (BoardSize+1) * (BoardSize+1)
 
-      matrix<ValueType> value;
+	matrix<ValueType> value;
 
-      // Conflict Vectors
-      matrix<bool> rows;
-      matrix<bool> cols;
-      matrix<bool> squares;
+	// Conflict Vectors
+	matrix<bool> rows;
+	matrix<bool> cols;
+	matrix<bool> squares;
 
 };
 
-board::board(int sqSize)
-   : value(BoardSize+1,BoardSize+1),
-     rows(BoardSize), cols(BoardSize), squares(BoardSize)
-// Board constructor
+template<typename T>
+ostream &operator<<(ostream &ostr, vector<T> &v)
+// Overloaded output operator for vector class.
 {
-   clear();
+	for (int i = 0; i < v.size(); i++)
+		ostr << v[i] << " ";
+	cout << endl;
+	return ostr;
+}
+
+board::board(int sqSize)
+	: value(BoardSize, BoardSize),
+	rows(BoardSize), cols(BoardSize), squares(BoardSize)
+	// Board constructor
+{
+	clear();
 }
 
 bool board::isSolved()
 {
-   bool boardFull = true, noColConflict = true, noRowConflict = true,
-        noSqConflict = true;
+	bool boardFull = true, noColConflict = true, noRowConflict = true,
+		noSqConflict = true;
 
-   for (int i = 1; i <= BoardSize; i++)
-   {
-      for (int j = 1; j <= BoardSize; j++)
-      {
-         boardFull = boardFull && value[i][j] != Blank;
-         noRowConflict = noRowConflict && rows[i - 1].at(j - 1);
-         noColConflict = noColConflict && cols[i - 1].at(j - 1);
-         noSqConflict = noSqConflict && squares[i - 1].at(j - 1);
-      }
-   }
+	for (int i = 1; i <= BoardSize; i++)
+	{
+		for (int j = 1; j <= BoardSize; j++)
+		{
+			boardFull = boardFull && value[i][j] != Blank;
+			noRowConflict = noRowConflict && rows[i - 1].at(j - 1);
+			noColConflict = noColConflict && cols[i - 1].at(j - 1);
+			noSqConflict = noSqConflict && squares[i - 1].at(j - 1);
+		}
+	}
 
-   return boardFull && noColConflict && noRowConflict && noSqConflict;
+	return boardFull && noColConflict && noRowConflict && noSqConflict;
+}
+
+void board::printConflicts()
+{
+	cout << "\nConflicts:\n";
+	cout << "Digit: 1 2 3 4 5 6 7 8 9" << endl << endl;
+	for (int i = 0; i < rows.rows(); i++)
+	{
+		cout << "Row " << i + 1 << ": " << rows[i];
+	}
+	cout << endl;
+	for (int i = 0; i < cols.rows(); i++)
+	{
+		cout << "Col " << i + 1 << ": " << cols[i];
+	}
+	cout << endl;
+	for (int i = 0; i < squares.rows(); i++)
+	{
+		cout << "Sqr " << i + 1 << ": " << squares[i];
+	}
+	cout << endl;
 }
 
 void board::clear()
 // Mark all possible values as legal for each board entry
 {
-   for (int i = 1; i <= BoardSize; i++)
-      for (int j = 1; j <= BoardSize; j++)
-      {
-         value[i][j] = Blank;
-      }
-}
-
-void board::initialize(ifstream &fin)
-// Read a Sudoku board from the input file.
-{
-   char ch;
-
-   clear();
-
-   for (int i = 1; i <= BoardSize; i++)
-   {
-      cols.resize(9,9);
-      squares.resize(9,9);
-      rows.resize(9,9);
-
-      for (int j = 1; j <= BoardSize; j++)
-	   {
-	       fin >> ch;
-
-          // If the read char is not Blank
-	      if (ch != '.')
-             setCell(i,j,ch-'0');   // Convert char to int
-      }
-   }
+	for (int i = 1; i <= BoardSize; i++)
+		for (int j = 1; j <= BoardSize; j++)
+		{
+			value[i - 1][j - 1] = Blank;
+		}
 }
 
 int squareNumber(int i, int j)
 // Return the square number of cell i,j (counting from left to right,
 // top to bottom.  Note that i and j each go from 1 to BoardSize
 {
-   // Note that (int) i/SquareSize and (int) j/SquareSize are the x-y
-   // coordinates of the square that i,j is in.
+	// Note that (int) i/SquareSize and (int) j/SquareSize are the x-y
+	// coordinates of the square that i,j is in.
 
-   return SquareSize * (i-1)/SquareSize + (j)%SquareSize;
+	return SquareSize * ((i - 1) / SquareSize) + (j - 1) / SquareSize + 1;
 }
 
-ostream &operator<<(ostream &ostr, vector<int> &v)
-// Overloaded output operator for vector class.
+void board::initialize(ifstream &fin)
+// Read a Sudoku board from the input file.
 {
-   for (int i = 0; i < v.size(); i++)
-      ostr << v[i] << " ";
-   cout << endl;
-   return ostr;
+	char ch;
+
+	cols.resize(BoardSize, MaxValue);
+	squares.resize(BoardSize, MaxValue);
+	rows.resize(BoardSize, MaxValue);
+	clear();
+
+	for (int i = 1; i <= BoardSize; i++)
+	{
+		for (int j = 1; j <= BoardSize; j++)
+		{
+			fin >> ch;
+
+			// If the read char is not Blank
+			if (ch != '.')
+				setCell(i, j, ch - '0');   // Convert char to int
+		}
+	}
 }
 
 void board::clearCell(int i, int j)
 // Sets the value in the cell to blank, updates the conflict list to remove
 // conflicts with this number in the column, row, and square
 {
-   if (i >= 1 && i <= BoardSize && j >= 1 && j <= BoardSize)
-   {
-      ValueType tmp = value[i][j];
-      value[i - 1][j - 1] = Blank;
-      cols[i - 1].at(tmp) = false;
-      rows[j - 1].at(tmp) = false;
-      squares[squareNumber(i, j)-1].at(tmp) = false;
-   }
-   else
-      throw rangeError("bad value in clearCell");
+	if (i >= 1 && i <= BoardSize && j >= 1 && j <= BoardSize)
+	{
+		ValueType tmp = value[i - 1][j - 1] - 1;
+		value[i - 1][j - 1] = Blank;
+		cols[i - 1].at(tmp) = false;
+		rows[j - 1].at(tmp) = false;
+		squares[squareNumber(i, j) - 1].at(tmp) = false;
+	}
+	else
+		throw rangeError("bad value in clearCell");
 }
 
 void board::setCell(int i, int j, int val)
 // Sets the value in a cell. Throws exception if bad values
 // are passed.
 {
-   if (i >= 1 && i <= BoardSize && j >= 1 && j <= BoardSize && val >= MinValue
-         && val <= MaxValue)
-   {
-      value[i][j] = val;
-      cols[i - 1].at(val) = true;
-      rows[j - 1].at(val) = true;
-	  int sqNum(squareNumber(i, j) - 1);
-      squares[sqNum].at(val) = true;
+	if (i >= 1 && i <= BoardSize && j >= 1 && j <= BoardSize && val >= MinValue
+		&& val <= MaxValue)
+	{
+		value[i - 1][j - 1] = val;
+		cols[j - 1].at(val - 1) = true;
+		rows[i - 1].at(val - 1) = true;
+		int sqNum(squareNumber(i, j) - 1);
+		squares[sqNum].at(val - 1) = true;
 
-   }
-   else
-      throw rangeError("bad value in setCell");
+	}
+	else
+		throw rangeError("bad value in setCell");
 }
 
 ValueType board::getCell(int i, int j)
 // Returns the value stored in a cell.  Throws an exception
 // if bad values are passed.
 {
-   if (i >= 1 && i <= BoardSize && j >= 1 && j <= BoardSize)
-      return value[i][j];
-   else
-      throw rangeError("bad value in getCell");
+	if (i >= 1 && i <= BoardSize && j >= 1 && j <= BoardSize)
+		return value[i - 1][j - 1];
+	else
+		throw rangeError("bad value in getCell");
 }
 
 bool board::isBlank(int i, int j)
 // Returns true if cell i,j is blank, and false otherwise.
 {
-   if (i < 1 || i > BoardSize || j < 1 || j > BoardSize)
-      throw rangeError("bad value in setCell");
+	if (i < 1 || i > BoardSize || j < 1 || j > BoardSize)
+		throw rangeError("bad value in setCell");
 
-   return (getCell(i,j) == Blank);
+	return (getCell(i, j) == Blank);
 }
 
 void board::print()
 // Prints the current board.
 {
-   for (int i = 1; i <= BoardSize; i++)
-   {
-      if ((i-1) % SquareSize == 0)
-      {
-         cout << " -";
-	      for (int j = 1; j <= BoardSize; j++)
-	         cout << "---";
-         cout << "-";
-	      cout << endl;
-      }
-      for (int j = 1; j <= BoardSize; j++)
-      {
-	      if ((j-1) % SquareSize == 0)
-	         cout << "|";
-	 if (!isBlank(i,j))
-	    cout << " " << getCell(i,j) << " ";
-	 else
-	    cout << "   ";
-      }
-      cout << "|";
-      cout << endl;
-   }
+	for (int i = 1; i <= BoardSize; i++)
+	{
+		if ((i - 1) % SquareSize == 0)
+		{
+			cout << " -";
+			for (int j = 1; j <= BoardSize; j++)
+				cout << "---";
+			cout << "-";
+			cout << endl;
+		}
+		for (int j = 1; j <= BoardSize; j++)
+		{
+			if ((j - 1) % SquareSize == 0)
+				cout << "|";
+			if (!isBlank(i, j))
+				cout << " " << getCell(i, j) << " ";
+			else
+				cout << "   ";
+		}
+		cout << "|";
+		cout << endl;
+	}
 
-   cout << " -";
-   for (int j = 1; j <= BoardSize; j++)
-      cout << "---";
-   cout << "-";
-   cout << endl;
+	cout << " -";
+	for (int j = 1; j <= BoardSize; j++)
+		cout << "---";
+	cout << "-";
+	cout << endl;
 }
 
 int main()
@@ -242,6 +263,15 @@ int main()
 			b1.initialize(fin);
 			b1.print();
 			b1.printConflicts();
+
+			if (b1.isSolved())
+			{
+				cout << "The board has been solved!" << endl;
+			}
+			else
+			{
+				cout << "The board has NOT been solved." << endl;
+			}
 		}
 	}
 	catch (indexRangeError &ex)
